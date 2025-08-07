8916f97243fc2e0e3227b97a77b7504db1b8e228
Kristian Johansen <kristian@vakwerk.org>
2025-08-07 14:04:16 +0200

    add updates

--- app/page.tsx
+++ app/page.tsx
@@ -1071,7 +1071,7 @@ const SectionAbout = ({
         mollit anim id est laborum.
       </div>
 
-      <div className="about-box absolute bottom-7 h-[60vh] lg:h-auto  lg:bottom-10 left-2 lg:left-3 flex justify-center items-center gap-2 lg:gap-4 will-change-transform opacity-100">
+      <div className="about-box absolute bottom-0 h-full lg:h-auto  lg:bottom-10 left-2 lg:left-3 flex justify-center items-center gap-2 lg:gap-4 will-change-transform opacity-100 pt-[130px] pb-[80px] lg:pb-0 lg:pt-0">
         <TeamMembers
           items={items}
           imageContainerRef={imageContainerRef}
