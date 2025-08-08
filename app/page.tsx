c7c8747219f41c64db79b5d729837e1f61058a74
Kristian Johansen <kristian@vakwerk.org>
2025-08-08 11:45:46 +0200

    add updates

--- app/page.tsx
+++ app/page.tsx
@@ -90,6 +90,28 @@ export default function ObserverPage() {
 
     intentRef.current.disable();
 
+    // pin swipe section and initiate observer
+    ScrollTrigger.create({
+      trigger: ".swipe-section",
+      pin: true,
+      start: "top top",
+      end: "+=200", // just needs to be enough to not risk vibration where a user's fast-scroll shoots way past the end
+      onEnter: (self) => {
+        if (intentRef.current.isEnabled) {
+          return;
+        } // in case the native scroll jumped past the end and then we force it back to where it should be.
+        self.scroll(self.start + 1); // jump to just one pixel past the start of this section so we can hold there.
+        intentRef.current.enable(); // STOP native scrolling
+      },
+      onEnterBack: (self) => {
+        if (intentRef.current.isEnabled) {
+          return;
+        } // in case the native scroll jumped backward past the start and then we force it back to where it should be.
+        self.scroll(self.end - 1); // jump to one pixel before the end of this section so we can hold there.
+        intentRef.current.enable(); // STOP native scrolling
+      },
+    });
+
     let currentX = 0;
     let targetX = 0;
     let animating = false;
